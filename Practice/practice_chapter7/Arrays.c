1379595bd3fa7d6a59cf5ec6b533695215320eef
rohitmalviya05 <malviyarohit180@gmail.com>
2026-01-20 22:19:17 +0530

    updated

---
+++ Practice/practice_chapter7/Arrays.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int marks[90];
+
+    marks[0] = 45;
+    marks[1] = 95;
+// we can go till Marks[89]
+    printf("The Marks 0 and Marks 1 is %d and %d", marks[0], marks[1]);
+    return 0;
+}
